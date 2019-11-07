16e3c39de93cb01a728341df6b8990162ee03977
github username <github email address>
2019-11-06 19:33:43 -0800

    user is able to search for a book and receive 5 books back. Now I'm going to work out the user's reading list.

--- app.rb
+++ app.rb
@@ -1,23 +1,70 @@
 require 'rest-client'
 require 'pry'
 
-puts "Please enter a book title"
-user_input = gets.chomp
 
-url = "https://www.googleapis.com/books/v1/volumes?q=#{user_input.gsub(" ", "+")}"
+# ------------ Book Searching ------------ 
 
-response = RestClient.get(url)
+puts "Welcome to ReadMe!"
+puts 
 
-json = JSON.parse(response)
+def search_query
+    puts "Please enter a book title:"
+    user_input = gets.chomp
 
-books = json['items']
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{user_input.gsub(" ", "+")}"
+    response = RestClient.get(url)
+    json = JSON.parse(response)
+    books = json['items']
+    show_books(books)
+end
 
-books.each do |book| 
-    title = book["items"][0]["volumeInfo"]["title"]
-    author = book["items"][0]["volumeInfo"]["author"]
-    publisher = book["items"][0]["volumeInfo"]["publisher"]
+def show_books(books)
+    max_list = []
+    puts "-------------------"
+
+    books.each do |book|
+        max_list << book
+    end
+
+
+    puts 
+    puts
+
+    puts "1) #{max_list[0]["volumeInfo"]["title"]}"
+    puts "  Author: #{max_list[0]["volumeInfo"]["authors"][0]}"
+    puts "  Publisher: #{max_list[0]["volumeInfo"]["publisher"]}"
+    puts
+    puts "2) #{max_list[1]["volumeInfo"]["title"]}"
+    puts "  Author: #{max_list[1]["volumeInfo"]["authors"][0]}"
+    puts "  Publisher: #{max_list[1]["volumeInfo"]["publisher"]}"
+    puts
+    puts "3) #{max_list[2]["volumeInfo"]["title"]}" 
+    puts "  Author: #{max_list[2]["volumeInfo"]["authors"][0]}"
+    puts "  Publisher: #{max_list[2]["volumeInfo"]["publisher"]}"
+    puts
+    puts "4) #{max_list[3]["volumeInfo"]["title"]}" 
+    puts "  Author: #{max_list[3]["volumeInfo"]["authors"][0]}"
+    puts "  Publisher: #{max_list[3]["volumeInfo"]["publisher"]}"
+    puts
+    puts "5) #{max_list[4]["volumeInfo"]["title"]}"
+    puts "  Author: #{max_list[4]["volumeInfo"]["authors"][0]}"
+    puts "  Publisher: #{max_list[4]["volumeInfo"]["publisher"]}"
+    save_book(max_list)
+end
+
+# ------------ Reading List ------------ 
+
+def save_book(max_list)
+    puts
+    puts "If you would like to save any of these books to your reading list
+    please select the number of the  book."
 end
 
 
+search_query
 
-Pry.start
+binding.pry 
+
+0
+
+
