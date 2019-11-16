aa8a86bf5711013d41ba1845ad84451862571f98
github username <github email address>
2019-11-16 06:46:15 -0800

    commented out the API test to try and do a mock instead.

--- spec/testing/mytest_spec.rb
+++ spec/testing/mytest_spec.rb
@@ -3,13 +3,13 @@ require 'rest-client'
 
 RSpec.describe "Mytest", :type => :controller do
 
-    describe "check API response" do
-        it "should return """ do
-            url = 'https://www.googleapis.com/books/v1/volumes?q=programming'
-            response = RestClient.get(url)
-            json = JSON.parse(response)
-            expect(json["kind"]).to eq("books#volumes")
-        end
-    end
+    # describe "check API response" do
+    #     it "should return """ do
+    #         url = 'https://www.googleapis.com/books/v1/volumes?q=programming'
+    #         response = RestClient.get(url)
+    #         json = JSON.parse(response)
+    #         expect(json["kind"]).to eq("books#volumes")
+    #     end
+    # end
 
 end
